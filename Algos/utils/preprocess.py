0e193d1775de3e5e67d497f2bbecb435679555a6
Yuvraj Garg <yuvraj97.ml@gmail.com>
2021-09-02 00:55:28 +0530

    Update `Linear_Regression`
    - Changed Input style
    - Makeinf function input more robust, now we can take mathematical functions as inputs.
    - Handline nan values

--- Algos/utils/preprocess.py
+++ Algos/utils/preprocess.py
@@ -3,40 +3,6 @@ import traceback
 import numpy as np
 
 
-def process_function_0(s: str):
-    """
-    Here we take a function expression as input then first we validate it
-    then we return a lambda function corresponding to that expression
-    :param s: str
-    :return:
-    """
-
-    if s == "": return False
-    allowed = ["+", "-", "*", "/", "**", " "]
-    s = s.replace("^", "**")
-
-    for c in s:
-        if not c.isnumeric() and c not in allowed and c != 'x':
-            return False
-
-    _s = s
-    for c in allowed:
-        _s = _s.replace(c, " ")
-    s_list = _s.split(" ")
-
-    args = set()
-    for e in s_list:
-        if e == "": continue
-        if 'x' == e[0]:
-            args.add(e)
-    args = sorted(args)
-    try:
-        return eval(f"lambda {', '.join(args)}: {s}")
-    except Exception as e:
-        print(e)
-        return False
-
-
 allowed_functions = set(dir(np.math))
 
 
