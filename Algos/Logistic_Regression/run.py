b6a427ca416750d714993f5c34f7fb324a297096
Yuvraj Garg <yuvraj97.ml@gmail.com>
2021-09-02 01:48:18 +0530

    Update `Logistic_Regression` improved inputs.

--- Algos/Logistic_Regression/run.py
+++ Algos/Logistic_Regression/run.py
@@ -15,111 +15,71 @@ def get_all_inputs() -> Dict[str, Union[str, int, float, List[float]]]:
     :return: Dict[str, Union[str, int, float]]
     """
 
-    method: str = st.selectbox("Which method you want to use", [
+    method: str = st.sidebar.selectbox("Which method you want to use", [
         "Batch Gradient Ascent",
         "Mini Batch Gradient Ascent"
     ])
 
-    seed: int = st.sidebar.number_input("Enter seed (-1 mean seed is disabled)", -1, 1000, 0, 1)
+    with st.sidebar.beta_expander("Generate n dimensional synthetic data"):
+        st.write("")
+        st_seed, st_n = st.beta_columns([1, 1])
+        seed: int = int(st_seed.text_input("Enter seed (-1 mean seed is disabled)", "0"))
+        n: int = int(st_n.text_input("N (number of training examples)", "100"))
+        n_features: int = int(st.text_input("Number of features", "2"))
+        st_lower_limit, st_upper_limit = st.beta_columns([0.5, 0.5])
+        lower_limit: float = float(st_lower_limit.text_input("Lower Limit", "-10.0"))
+        upper_limit: float = float(st_upper_limit.text_input("Upper Limit", "10.0"))
 
-    st.sidebar.write("### Gaussian Noise")
-    st_noise = st.sidebar.empty()
-    st_mean, st_std = st.sidebar.beta_columns([1, 1])
-    mean: float = st_mean.slider("Mean", -100.0, 100.0, 0.0, 10.0)
-    std: float = st_std.slider("Standard deviation", 0.0, 5.0, 1.0, 0.1)
-    st_noise.markdown(f"$\\mathcal{{N}}(\\mu= {mean}, \\sigma^2={std}^2)$")
+        st.write("### Classes proportions")
+        st_prop = st.beta_columns([1, 1])
+        classes_proportions = [
+            float(st_prop[0].text_input(f"Class: 1", "0.5")),
+            float(st_prop[1].text_input(f"Class: 2", "0.5"))
+        ]
 
-    st.sidebar.write("### Logistic Regression Parameters")
+        if not math.isclose(sum(classes_proportions), 1.0, abs_tol=0.01):
+            st.error("Proportions should sum to $1$")
+            raise ValueError("Algos.Logistic_Regression.run: Proportions should sum to $1$")
 
-    # st_n_classes, st_n_features = st.sidebar.beta_columns([1, 1])
-    n_classes = 2
-    # n_classes: int = st_n_classes.number_input(
-    #     "Number of classes",
-    #     min_value=2,
-    #     max_value=100,
-    #     value=2,
-    #     step=1)
+        st.write("### Gaussian Noise $\\mathcal{N}(\\mu,\\sigma^2)$")
+        st_mean, st_std = st.beta_columns([1, 1])
+        mean: float = float(st_mean.text_input("Mean", "0.0"))
+        std: float = float(st_std.text_input("Standard deviation", "1.0"))
 
-    n_features: int = st.sidebar.number_input(
-        "Number of Features",
-        min_value=1,
-        max_value=100,
-        value=2,
-        step=1)
+    with st.sidebar.beta_expander("Linear Regression Parameters", True):
 
-    st.sidebar.write("### Classes proportions")
-    classes_proportions = []
-    st_classes_proportions = [st.sidebar.beta_columns([1] * 3) for _ in range(n_classes // 3 + 1)]
+        st_lr, st_epsilon, st_epochs = st.beta_columns([1, 1, 0.8])
+        lr: float = float(st_lr.text_input("Learning Rate", "0.01"))
+        epochs: int = int(st_epochs.text_input("epochs", "50"))
+        epsilon: float = float(st_epsilon.text_input("Epsilon", "0.05"))
 
-    j = 0
-    for j in range(n_classes // 3):
-        for i in range(3):
-            classes_proportions.append(
-                float(
-                    st_classes_proportions[j][i].text_input(
-                        f"Class: {3 * j + i + 1}",
-                        "{:.3f}".format(1/n_classes),
-                        key=f"proportions-{j}-{i}"
-                    )
-                )
-            )
-    for i in range(n_classes % 3):
-        classes_proportions.append(
-            float(
-                st_classes_proportions[-1][i].text_input(
-                    f"Class: {j * 3 + i + 1}",
-                    "{:.3f}".format(1 / n_classes),
-                    key=f"proportions-{-1}-{i}"
-                )
-            )
-        )
+        batch_size = None
+        if method == "Mini Batch Gradient Descent":
+            batch_size = st.number_input("Batch size", 1, n, 10, 1)
 
-    if not math.isclose(sum(classes_proportions), 1.0, abs_tol=0.01):
-        st.error("Proportions should sum to $1$")
-        raise ValueError("Algos.Logistic_Regression.run: Proportions should sum to $1$")
+        lr_method: str = st.radio("Choose method", ["Implementation From Scratch", "PyTorch Implementation"])
+        sim_method: str = st.radio("", ["Simulate", "Manually Increment Steps"], key="Algos-LR-Sim-Step")
 
-    st_n, st_lr = st.sidebar.beta_columns([1, 1])
-    n: int = st_n.slider("N", 10, 1000, 100, 10)
-    lr: float = st_lr.slider("Learning Rate", 0.1, 10.00, 0.5, 0.1)
-    st_n, st_lr = st.sidebar.beta_columns([1, 1])
-    st_n.success(f"$N:{n}$")
-    st_lr.success(f"$lr:{lr}$")
-
-    st_epochs, st_epsilon = st.sidebar.beta_columns([1, 1])
-    epochs: int = st_epochs.slider("epochs", 1, 100, 50, 10)
-    epsilon: float = st_epsilon.slider("Epsilon", 0.001, 0.1, 0.05, 0.001)
-    st_epochs, st_epsilon = st.sidebar.beta_columns([1, 1])
-    st_epochs.success(f"epochs$:{epochs}$")
-    st_epsilon.success(f"$\\epsilon:{epsilon}$")
-
-    batch_size = None
-    if method == "Mini Batch Gradient Ascent":
-        batch_size = st.sidebar.number_input("Batch size", 1, n, 10, 1)
-
-    st.sidebar.write("-----")
-
-    lr_method: str = st.sidebar.radio("Choose method", ["Implementation From Scratch", "PyTorch Implementation"])
-    sim_method: str = st.sidebar.radio("", ["Simulate", "Manually Increment Steps"], key="Algos-LR-Sim-Step")
-
-    sim_button, step_button = None, None
-    if sim_method == "Simulate":
-        sim_button = st.sidebar.button("Run Simulation")
-    else:
-        step_button = st.sidebar.button("Step")
+        sim_button, step_button = None, None
+        if sim_method == "Simulate":
+            sim_button = st.button("Run Simulation")
+        else:
+            step_button = st.button("Step")
 
     d = {
         "method": method,
-        "n_classes": n_classes,
-        "n_features": n_features,
-        "classes_proportions": classes_proportions,
-        # "do_normalization": do_normalization,
+        "seed": seed,
         "n": n,
+        "n_classes": 2,
+        "n_features": n_features,
+        "lower_limit": lower_limit,
+        "upper_limit": upper_limit,
+        "classes_proportions": classes_proportions,
         "mean": mean,
         "std": std,
-        "seed": seed,
-        "epochs": epochs,
         "lr": lr,
         "epsilon": epsilon,
+        "epochs": epochs,
         "batch_size": batch_size,
         "lr_method": lr_method,
         "sim_method": sim_method,
