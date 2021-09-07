4c77ca486457374f7277bd3cc3377751f3e8af8e
Yuvraj Garg <yuvraj97.ml@gmail.com>
2021-09-08 02:10:43 +0530

    Update `Linear_Regression->auto_simulation`, using `plot_predition()`

--- Algos/Linear_Regression/simulation/auto_simulation.py
+++ Algos/Linear_Regression/simulation/auto_simulation.py
@@ -1,7 +1,8 @@
 import time
 import streamlit as st
-from plotly.graph_objs import Figure
-from Algos.utils.plots import plotly_plot, mesh3d
+
+from Algos.Linear_Regression.utils import plot_predition
+from Algos.utils.plots import plotly_plot
 
 
 def run(f, plt, inputs: dict):
@@ -16,9 +17,6 @@ def run(f, plt, inputs: dict):
 
     st_theta, st_error, st_plot = st.sidebar.empty(), st.empty(), st.empty()
     st_theta_completed = st.empty()
-
-    min_X, max_X = inputs["X"][:, 0].min(), inputs["X"][:, 0].max()
-    n, d = inputs["X"].shape
 
     errors = []
     epochs = []
@@ -44,48 +42,7 @@ def run(f, plt, inputs: dict):
             ).replace('x_0', '')}$            
             """)
 
-        if d == 1:
-            new_fig: Figure = plotly_plot(
-                [min_X, max_X],
-                [
-                    theta[0][0] + theta[1][0] * min_X,
-                    theta[0][0] + theta[1][0] * max_X
-                ],
-                fig=plt,
-                mode="lines",
-                color="blue",
-                do_not_change_fig=True,
-                title=f"Linear Regression (epoch: {epoch})"
-            )
-            st_plot.plotly_chart(new_fig)
-        elif d == 2:
-            description = {
-                "title": {
-                    "main": f"Linear Regression (epoch: {epoch})",
-                    "x": "x1",
-                    "y": "x2",
-                    "z": "y"
-                },
-                "label": {
-                    "main": "",
-                },
-                "hovertemplate": "(x1, x1): (%{x}, %{y})<br>f(%{x}, %{y}): %{z}"
-            }
-            min_X2, max_X2 = inputs["X"][:, 1].min(), inputs["X"][:, 1].max()
-            new_fig: Figure = mesh3d(
-                [min_X, min_X, max_X, max_X],
-                [min_X2, max_X2, min_X2, max_X2],
-                [
-                    theta[0][0] + theta[1][0] * min_X + theta[2][0] * min_X2,
-                    theta[0][0] + theta[1][0] * min_X + theta[2][0] * max_X2,
-                    theta[0][0] + theta[1][0] * max_X + theta[2][0] * min_X2,
-                    theta[0][0] + theta[1][0] * max_X + theta[2][0] * max_X2,
-                ],
-                description,
-                fig=plt,
-                opacity=0.9
-            )
-            st_plot.plotly_chart(new_fig)
+            st_plot.plotly_chart(plot_predition(inputs["X"], theta, plt))
 
         errors.append(error)
         epochs.append(epoch)
