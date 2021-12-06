9a53fc64d14502511c58205bc9e0bdd2fa8e5240
Yuvraj Garg <yuvraj97.ml@gmail.com>
2021-12-06 22:56:14 +0530

    Update utils.py

--- Algos/utils/utils.py
+++ Algos/utils/utils.py
@@ -2,7 +2,12 @@ import streamlit as st
 
 
 def intialize(title: str):
-    # st.set_page_config(layout='centered', initial_sidebar_state='expanded')
+    st.set_page_config(
+        page_title=title,
+        page_icon="♾️",
+        layout="wide",  # centered
+        initial_sidebar_state="expanded",
+    )
     st.sidebar.markdown(
         f"""
         <a rel='noreferrer' target='_blank' href="https://app.quantml.org/">
@@ -25,7 +30,10 @@ def hamburger_correction():
     st.markdown("""
     <style>
     /* Set the top padding */
-    .css-hi6a2p {padding-top: 1rem;}
+    .block-container
+    {
+        padding-top: 2rem;
+    }
 
     /* This is to hide Streamlit footer */
     footer {visibility: hidden;}
@@ -33,12 +41,11 @@ def hamburger_correction():
     If you did not hide the hamburger menu completely,
     you can use the following styles to control which items on the menu to hide.
     */
-    ul[data-testid=main-menu-list] > li:nth-of-type(3), /* Deploy this app */
-    ul[data-testid=main-menu-list] > li:nth-of-type(5), /* Documentation */
-    ul[data-testid=main-menu-list] > li:nth-of-type(6), /* Ask a question */
-    ul[data-testid=main-menu-list] > li:nth-of-type(7), /* Report a bug */
-    ul[data-testid=main-menu-list] > li:nth-of-type(8), /* Streamlit for Teams= */
-    ul[data-testid=main-menu-list] > li:nth-of-type(10), /* About */
+    ul[data-testid=main-menu-list] > li:nth-of-type(4), /* Documentation */
+    ul[data-testid=main-menu-list] > li:nth-of-type(5), /* Ask a question */
+    ul[data-testid=main-menu-list] > li:nth-of-type(6), /* Report a bug */
+    ul[data-testid=main-menu-list] > li:nth-of-type(7), /* Streamlit for Teams= */
+    ul[data-testid=main-menu-list] > li:nth-of-type(9), /* About */
     ul[data-testid=main-menu-list] > div:nth-of-type(1), /* 1st divider */
     ul[data-testid=main-menu-list] > div:nth-of-type(2), /* 2nd divider */
     ul[data-testid=main-menu-list] > div:nth-of-type(3) /* 3rd divider */
@@ -47,7 +54,7 @@ def hamburger_correction():
     /* Sidebar */
     section[data-testid=stSidebar] > div
     {
-        padding-top: 3.5rem;
+        padding-top: 1.5rem;
     }
 
     </style>
@@ -56,32 +63,23 @@ def hamburger_correction():
 
 def footer():
     st.sidebar.write("-----")
-    # st.sidebar.write("""
-    # If you like this project, <br> then give it a ⭐ on [GitHub](https://github.com/yuvraj97/MLViz)
-    # <iframe
-    #     src="https://ghbtns.com/github-btn.html?user=yuvraj97&repo=MLViz&type=star&count=true&size=large"
-    #     frameborder="0" scrolling="0" width="170" height="30" title="GitHub">
-    # </iframe>""", unsafe_allow_html=True)
 
-    st.sidebar.markdown("## Connect")
-    # st.sidebar.write("""
-    # <iframe
-    #     src="https://ghbtns.com/github-btn.html?user=yuvraj97&type=follow&count=true&size=large"
-    #     frameborder="0" scrolling="0" width="250" height="30" title="GitHub">
-    # </iframe>""", unsafe_allow_html=True)
-    st.sidebar.markdown("""
-    [Donate Here if you like this project](http://www.quantml.org/donate)    
-    LinkedIn: [yuvraj97](https://www.linkedin.com/in/yuvraj97/)    
-    Github: [yuvraj97](https://github.com/yuvraj97/)    
-    Email: [yuvraj@quantml.org](mailto:yuvraj@quantml.org)
+    st.sidebar.write("""
+    If you like this project, <br> then give it a ⭐ on [GitHub](https://github.com/yuvraj97/MLViz)
+    <iframe
+        src="https://ghbtns.com/github-btn.html?user=yuvraj97&repo=MLViz&type=star&count=true&size=large"
+        frameborder="0" scrolling="0" width="170" height="30" title="GitHub">
+    </iframe>
+    
+    <iframe
+        src="https://ghbtns.com/github-btn.html?user=yuvraj97&type=follow&count=true&size=large"
+        frameborder="0" scrolling="0" width="250" height="30" title="GitHub">
+    </iframe>
+    
+    [![Patreon](https://img.shields.io/endpoint.svg?url=https%3A%2F%2Fshieldsio-patreon.vercel.app%2Fapi%3Fusername%3Dquantml%26type%3Dpatrons&style=for-the-badge)](https://patreon.com/quantml)    
+    [![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/yuvraj97/)    
+    **Email: [`yuvraj@quantml.org`](mailto:yuvraj@quantml.org)**  
     """, unsafe_allow_html=True)
-
-    # with st.sidebar.container():
-    #     st.markdown(
-    #         "![Yuvraj's GitHub stats](https://github-readme-stats.vercel.app/api?username=yuvraj97"
-    #         "&show_icons=true&theme=radical&include_all_commits=true&count_private=true)",
-    #         unsafe_allow_html=True
-    #     )
 
 
 def reset_session():
